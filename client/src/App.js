170f351cf9320627e28d7595c01f15712cdaa6cd
Sooraj Rajkumar <soorajrk007@gmail.com>
2023-07-08 16:28:20 +0530

    initial commit

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import ChatGPT from './components/chatgpt';
+
+function App() {
+  return (
+    <div className="App">
+     <ChatGPT/>
+    </div>
+  );
+}
+
+export default App;
---
+++ server/index.js
@@ -0,0 +1,41 @@
+const express = require("express");
+
+const cors = require("cors");
+
+require('dotenv').config();
+
+const bodyParser = require("body-parser");
+
+const { Configuration, OpenAIApi } = require("openai");
+
+const config = new Configuration({
+    apiKey: process.env.OPENAI_API_KEY
+});
+
+const openai = new OpenAIApi(config);
+
+// Setup server
+
+const app = express();
+app.use(bodyParser.json());
+app.use(cors());
+
+// endpoint for ChatGPT
+
+app.post("/chat", async (req, res) => {
+  const { prompt } = req.body;
+
+  const completion = await openai.createCompletion({
+    model: "text-davinci-003",
+    max_tokens: 512,
+    temperature: 0,
+    prompt: prompt,
+  });
+  res.send(completion.data.choices[0].text);
+});
+
+const PORT = 8080;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port: ${PORT}`);
+});
